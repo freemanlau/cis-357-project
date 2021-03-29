329602efed4a0b26bfa2346d8e45d1196da95d5d
Kyle Ronayne <kyleronaynep@gmail.com>
2021-03-29 17:37:00 -0400

    Reformatted; Added background color

--- newsapp/LoginView.swift
+++ newsapp/LoginView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct LoginView: View {
-
+    let loginFieldColor = Color(red: 239/255, green: 243/255, blue: 244/255, opacity: 1)
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        VStack {
-            Image("Icon")
-                .resizable()
-                .aspectRatio(contentMode: ContentMode.fit)
-                .frame(width: 74.0, height: 74.0)
-                .padding(Edge.Set.bottom, 20.0)
-            Text("Login").bold().font(.title)
+        ZStack {
+            Color.init(UIColor(red: 1.00, green: 0.97, blue: 0.97, alpha: 1.00)).ignoresSafeArea()
+            VStack {
+                // App icon
+                Image("Icon")
+                    .resizable()
+                    .aspectRatio(contentMode: ContentMode.fill)
+                    .frame(width: 150, height: 150)
+                    .clipped()
+                
+                // Login title and subtitle
+                Text("Welcome!").bold().font(.title)
+                Text("A new way to get your news")
+                    .font(.subheadline)
+                    .padding(.bottom, 70)
+                
+                // Username field
+                TextField("Username", text: $username)
+                    .padding()
+                    .background(loginFieldColor)
+                    .cornerRadius(5)
+                    .padding(20)
+            }
         }
     }
 }
